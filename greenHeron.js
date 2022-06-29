1ebab55889dcd4fd707c7ce8e42e0370643d9712
Alex Waller <alex@artofalexwaller.com>
2022-06-28 23:44:37 -0400

    Array22.delete()

--- greenHeron.js
+++ greenHeron.js
@@ -8,7 +8,8 @@ class Array22 {
 
   push(element) {
     this.container[this.length] = element;
-    return this.length++;
+    this.length++;
+    return this.length;
   }
 
   pop() {
@@ -100,3 +101,71 @@ class Array22 {
     return rElement;
   }
 }
+
+const robots = new Array22();
+
+robots.push("Hank-44");
+robots.push("Warren-21");
+robots.push("Mellon-Tech");
+robots.push("Eggplant-Head");
+
+console.log(robots.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Mellon-Tech', 3: 'Eggplant-Head'}
+console.log(robots.length);
+// 4
+console.log(robots.push("Bender"));
+// 5
+console.log(robots.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Mellon-Tech', 3: 'Eggplant-Head', 4: 'Bender'}
+console.log(robots.pop());
+// Bender
+console.log(robots.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Mellon-Tech', 3: 'Eggplant-Head'}
+console.log(robots.length);
+// 4
+console.log(robots.indexOf("Hank-44"));
+// 0
+console.log(robots.indexOf("Warren-21"));
+// 1
+console.log(robots.indexOf("Mellon-Tech"));
+// 2
+console.log(robots.indexOf("Eggplant-Head"));
+// 3
+console.log(robots.indexOf("Bender"));
+// -1
+console.log(robots.includes("Hank-44"));
+// true
+console.log(robots.includes("Eggplant-Head"));
+// true
+console.log(robots.includes("Bender"));
+// false
+let roboClone1 = robots.slice(1, 3);
+
+console.log(roboClone1.container);
+// {0: 'Warren-21', 1: 'Mellon-Tech'}
+console.log(roboClone1.length);
+// 2
+console.log(roboClone1.shift());
+// Warren-21
+console.log(roboClone1.container);
+// {0: 'Mellon-Tech'}
+console.log(roboClone1.length);
+// 1
+console.log(roboClone1.unshift("Hank-44", "Warren-21", "Bender"));
+// 4
+console.log(roboClone1.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Bender', 3: 'Mellon-Tech'}
+console.log(roboClone1.push("Test 1"));
+// 5
+console.log(roboClone1.push("Eggplant-Head"));
+// 6
+console.log(roboClone1.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Mellon-Tech', 3: 'Eggplant-Head', 4: 'Test 1', 5: 'Eggplant-Head'}
+console.log(roboClone1.length);
+// 6
+console.log(roboClone1.delete(4));
+// Test 1
+console.log(roboClone1.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Bender', 3: 'Mellon-Tech', 4: 'Eggplant-Head'}
+console.log(roboClone1.length);
+// 5
