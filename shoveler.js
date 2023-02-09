93f1faa89bbed1e4248798a31c1a05a7090c6cf0
Alex Waller <alex@artofalexwaller.com>
2023-02-08 23:55:35 -0500

    RoboArray

--- shoveler.js
+++ shoveler.js
@@ -61,3 +61,87 @@ function reverseNum(num) {
 console.log(reverseNum(23));
 // 32
 console.log(reverseNum(189));
+// 981
+console.log(reverseNum(123456));
+// 654321
+console.log("----------------------------------------------------");
+console.log("----------------------------------------------");
+
+function mathDotSqrt(num, power) {
+  let i = 1;
+  let num2 = num;
+  for (; i < power; i++) {
+    num = num * num2;
+  }
+  return num;
+}
+
+console.log(mathDotSqrt(3, 3));
+// 27
+console.log(mathDotSqrt(4, 3));
+// 64
+console.log(mathDotSqrt(9, 3));
+// 729
+console.log(81 * 9);
+// 729
+console.log("-------------------------------------------------");
+console.log("-------------------------------------------------");
+
+let racecar = "racecar";
+
+let lamp = "lamp";
+
+let radar = "radar";
+
+let television = "television";
+
+let kayak = "kayak";
+
+let computer = "computer";
+
+function palindrome(string) {
+  let i = 0;
+  let j = string.length - 1;
+  for (; i < j; i++, j--) {
+    if (string.charAt(i) !== string.charAt(j)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(palindrome(racecar));
+// true
+console.log(palindrome(lamp));
+// false
+console.log(palindrome(radar));
+// true
+console.log(palindrome(television));
+// false
+console.log(palindrome(kayak));
+// true
+console.log(palindrome(computer));
+// false
+console.log("----------------------------------------------");
+console.log("-----------------------------------------");
+
+class RoboArray {
+  constructor(message = "RoboArray!") {
+    this.container = {};
+    this.length = 0;
+    this.message = message;
+  }
+
+  push(element) {
+    this.container[this.length] = element;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    let rElement = this.container[this.length - 1];
+    delete this.container[this.length - 1];
+    this.length--;
+    return rElement;
+  }
+}
