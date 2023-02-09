37653d2b8f6f1632d18aa3a7d3c573dd808b8c0e
Alex Waller <alex@artofalexwaller.com>
2023-02-08 23:23:10 -0500

    reverseNum()

--- shoveler.js
+++ shoveler.js
@@ -18,3 +18,46 @@ console.log(mathDotRound(91.1));
 // 91
 console.log("------------------------------------------");
 console.log("---------------------------------------");
+
+let twentyThree = 23;
+
+let thirtyTwo = twentyThree % 10;
+
+console.log(thirtyTwo);
+// 3
+twentyThree = (twentyThree - (twentyThree % 10)) / 10;
+
+console.log(twentyThree);
+// 2
+twentyThree = twentyThree % 10;
+
+console.log(twentyThree);
+
+function reverseNum(num) {
+  // num = 189
+  let rNum = 0;
+  for (; Math.round(num) > 0; num = num / 10) {
+    let remainder = num % 10;
+    // 189 % 10 = 9
+    // 18 % 10 = 8
+    // 1 % 10 = 1
+    rNum = rNum * 10 + remainder;
+    // 0 * 10 + 9 = 9
+    // 9 * 10 + 8 = 98
+    // 98 * 10 + 1 = 981
+    num = num - (num % 10);
+    // 189 - (189 % 10) = 180
+    // 18 - (18 % 10) = 10
+    // 1 - (1 % 10) = 1
+
+    // 180 / 10 = 18
+    // 10 / 10 = 1
+    // 1 / 10 = 0.10
+    // loop breaks because 0.10 rounded is not greater than zero
+  }
+  return rNum;
+}
+
+console.log(reverseNum(23));
+// 32
+console.log(reverseNum(189));
