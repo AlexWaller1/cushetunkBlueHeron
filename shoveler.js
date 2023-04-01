55a4508d70871747f061341a7d899fcc224b10ab
Alex Waller <alex@artofalexwaller.com>
2023-04-01 01:07:42 -0400

    RobotsNJ.push()

--- shoveler.js
+++ shoveler.js
@@ -154,4 +154,78 @@ class RoboArray21 {
     }
     return -1;
   }
+
+  includes(element) {
+    let i = 0;
+    for (; i < this.length; i++) {
+      if (this.container[i] === element) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  slice(index1, index2) {
+    let clone = new RoboArray21("RoboArray21 Clone!");
+    for (; index1 < index2; index1++) {
+      clone.push(this.container[index1]);
+    }
+    return clone;
+  }
+
+  shift() {
+    let rElement = this.container[0];
+    let newArray = new RoboArray21();
+    let i = 1;
+    for (; i < this.length; i++) {
+      newArray.push(this.container[i]);
+    }
+    this.container = newArray.container;
+    this.length = newArray.length;
+    return rElement;
+  }
+
+  unshift(args) {
+    let newArray = new RoboArray21();
+    let i = 0;
+    for (; i < arguments.length; i++) {
+      newArray.push(arguments[i]);
+    }
+    let j = 0;
+    for (; j < this.length; j++) {
+      newArray.push(this.container[j]);
+    }
+    this.container = newArray.container;
+    this.length = newArray.length;
+    return this.length;
+  }
+
+  delete(index) {
+    let rElement = this.container[index];
+    let newArray = new RoboArray21();
+    let i = 0;
+    for (; i < this.length; i++) {
+      if (i !== index) {
+        newArray.push(this.container[i]);
+      }
+    }
+    this.container = newArray.container;
+    this.length = newArray.length;
+    return rElement;
+  }
 }
+
+const RobotsNJ = new RoboArray21();
+
+console.log(RobotsNJ.push("Hank-44"));
+// 1
+console.log(RobotsNJ.push("Warren-21"));
+// 2
+console.log(RobotsNJ.push("Mellon-Tech"));
+// 3
+console.log(RobotsNJ.push("Eggplant-Head"));
+// 4
+console.log(RobotsNJ.push("test"));
+// 5
+console.log(RobotsNJ.container);
+// {0: 'Hank-44', 1: 'Warren-21', 2: 'Mellon-Tech', 3: 'Eggplant-Head', 4: 'test'}
